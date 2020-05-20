6e770c4e20b57f36218e2adfe9fe7b4f8051bab5
chaltenio <dev@chalten.io>
2020-05-20 19:21:26 +1200

    Changes in the form

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'; 
 
 export class AddProduct extends Component {
+    state = {
+        description: ''
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addProduct(this.state.description);
+        this.setState({ description: ''});
+    }
+
+    onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
     render() {
         return (
-          <form style={{ display: 'flex' }}>
+          <form onSubmit={this.onSubmit} style={{ display: 'flex' }}>
             <input
               type="text"
-              name="Description"
+              name="description"
               style={{ flex: '10', padding: '5px' }}
               placeholder="Add Product ..."
+              value={this.state.description}
+              onChange={this.onChange}
             />
 
             <input type="submit" value="Submit" className="btn" style={{flex: '1'}}/>
@@ -16,5 +31,11 @@ export class AddProduct extends Component {
         );
     }
 }
+
+// PropTypes
+AddProduct.propTypes = {
+  addProduct: PropTypes.func.isRequired
+}
+
 
 export default AddProduct
